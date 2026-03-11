9d86ebab198af5362a30ec8ea7bf5fc8cd744147
izaack55182 <L20TE0298@TEPEACA.TECNM.MX>
2026-03-11 16:17:47 -0600

    feat: add PublicLayout component with main content area and footer.

--- app/routes/layout/components/public-layout.tsx
+++ app/routes/layout/components/public-layout.tsx
@@ -1,12 +1,10 @@
 // app/layouts/public-layout.tsx
 
 import { Footer } from '@/routes/layout/components/footer'
-import { Header } from './public-header'
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="flex flex-col min-h-screen">
-			<Header />
 			<main className="flex-1 pt-16">{children}</main>
 			<Footer />
 		</div>
