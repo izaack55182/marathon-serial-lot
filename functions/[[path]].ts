3f7b06ace70dd00759910585c4ee0192f36a1382
izaack55182 <L20TE0298@TEPEACA.TECNM.MX>
2026-03-11 17:01:08 -0600

    fix: pass getLoadContext to cloudflare adapter

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,6 @@
 import handle from 'hono-react-router-adapter/cloudflare-pages'
 // @ts-ignore - build will be present after react-router build
 import * as build from '../build/server'
-import server from '../server'
+import server, { getLoadContext } from '../server'
 
-export const onRequest = handle(build, server)
+export const onRequest = handle(build, server, { getLoadContext })
